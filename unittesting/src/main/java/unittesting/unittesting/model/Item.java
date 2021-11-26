b53321f271482877be719f6d630c0c83e21a2d6f
KacperGierycz <kacpergierycz@gmail.com>
2021-11-26 16:10:01 +0100

    database h2 setup done hibernate done 1 item inserted

--- unittesting/src/main/java/unittesting/unittesting/model/Item.java
+++ unittesting/src/main/java/unittesting/unittesting/model/Item.java
@@ -1,13 +1,28 @@
 package unittesting.unittesting.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
 
-
+@Entity
 public class Item {
 
+	@Id
 	private int id;
 	private String name;
 	private int price;
 	private int quantity;
+	
+	@Transient
+	private int value;
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
 
 	public Item(int id,String name, int price, int quantity) {
 		this.id=id;
