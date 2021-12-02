9bb0ccc63b19fe280a2b82f693b30ccfc1b9d2cb
KacperGierycz <kacpergierycz@gmail.com>
2021-12-02 14:17:02 +0100

    individual tests properties  and little functional programing

--- unittesting/src/main/java/unittesting/unittesting/business/SomeBusinessImpl.java
+++ unittesting/src/main/java/unittesting/unittesting/business/SomeBusinessImpl.java
@@ -1,5 +1,8 @@
 package unittesting.unittesting.business;
 
+import java.util.Arrays;
+import java.util.OptionalInt;
+
 import unittesting.unittesting.data.SomeDataService;
 
 public class SomeBusinessImpl {
@@ -16,16 +19,24 @@ public class SomeBusinessImpl {
 
 	public int calculate(int[] data) {
 		
-		int sum=0;
-		for(int value:data) {
-			sum+=value;
-		}
-		return sum;
+		return Arrays.stream(data).reduce(Integer::sum).orElse(0);
+		
+//		OptionalInt number= Arrays.stream(data).reduce(Integer::sum);
+//		return number.orElse(0);
+		
+//		int sum=0;
+//		for(int value:data) {
+//			sum+=value;
+//		}
+//		return sum;
 		
 	}
 	
 	
 	public int calculateSumUsingDataService() {
+		
+		
+
 		
 		int sum=0;
 		int[]data=someDataService.retrieveAllData();
